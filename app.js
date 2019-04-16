c0515ab72724535d0dce32f64da5406112273ccd
周其军 <qijun.zhou@changhong.com>
2019-04-16 11:02:30 +0800

    配置babel

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let index = require('./src/index/index.js');//commonJS 模块 
 import b from "./src/b.js";// es6 模块
 import  './src/index.css'
+import  iAmJavascriptES6 from './src/es6'
+iAmJavascriptES6()
 index();
 b();
---
+++ src/es6.js
@@ -0,0 +1,3 @@
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+export default iAmJavascriptES6;
