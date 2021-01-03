746b0a1abe24700f9c40fa6053ce8bdee14f6248
jack <jackzhoumine@gmail.com>
2021-01-03 18:11:12 +0800

    修改添加图片的位置

--- src/createImg.js
+++ src/createImg.js
@@ -2,15 +2,15 @@
  * @Description: 创建图片
  * @Date: 2020-06-21 04:33:29
  * @Author: JackChouMine
- * @LastEditTime: 2020-06-21 05:21:25
- * @LastEditors: JackChouMine
+ * @LastEditTime: 2021-01-03 18:09:43 +0800
+ * @LastEditors: JackChou
  */
 import style from './scss/img.scss'
 import macPng from './img/mac.png'
 export default function createImg() {
-  const img = new Image()
-  img.src = macPng
-  img.classList.add(style.img)
-  const body = document.querySelector('body')
-  body.appendChild(img)
+	const img = new Image()
+	img.src = macPng
+	img.classList.add(style.img)
+	const body = document.querySelector('#app')
+	body.appendChild(img)
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
  * @Description: vue 入口文件
  * @Date: 2020-06-18 01:25:40
  * @Author: JackChouMine
- * @LastEditTime: 2020-06-26 01:25:43
- * @LastEditors: JackChouMine
+ * @LastEditTime: 2021-01-03 18:10:34 +0800
+ * @LastEditors: JackChou
  */
 
 import Vue from 'vue'
@@ -15,13 +15,17 @@ import regular from '@fortawesome/fontawesome-free-regular'
 import brands from '@fortawesome/fontawesome-free-brands'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import createImg from './createImg'
-createImg()
+// createImg()
 library.add(solid)
 library.add(regular)
 library.add(brands)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
-  el: '#hello',
-  render: (h) => h(App),
+	el: '#hello',
+	render: (h) => h(App),
+	mounted() {
+		// 再根组件挂载之后，追加一张图片
+		createImg()
+	},
 })
