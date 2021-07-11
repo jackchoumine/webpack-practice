e312563d06046a8de5497fd8377a12fefc8fb4ba
jack <jackzhoumine@gmail.com>
2021-07-11 23:54:37 +0800

    配置less-load

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@
  * @Description: 入口文件
  * @Date: 2021-07-11 22:03:32 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-07-11 23:34:05 +0800
+ * @LastEditTime: 2021-07-11 23:45:47 +0800
  * @LastEditors: JackChou
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './css/index.css'
+import './less/index.less'
 class App extends React.Component {
   render() {
-    return <div>hello </div>
+    return (
+      <div className='head'>
+        hello
+        <h1>world</h1>
+      </div>
+    )
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Description: webpack 配置
  * @Date: 2021-07-11 22:00:35 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-07-11 23:17:41 +0800
+ * @LastEditTime: 2021-07-11 23:46:13 +0800
  * @LastEditors: JackChou
  */
 const { resolve } = require('path')
@@ -22,6 +22,14 @@ module.exports = {
         include: resolve(__dirname, 'src'),
         exclude: resolve(__dirname, 'node_modules'),
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
     ],
   },
