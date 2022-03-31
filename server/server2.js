a6a5e0a4a44d8942bf1211c2645bcb44613b7cd5
jack <jackzhoumine@gmail.com>
2022-04-01 00:03:27 +0800

    feat: express 渲染 nunjs模板

--- server/server2.js
+++ server/server2.js
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2022-03-31 23:18:18 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-03-31 23:18:18 +0800
+ * @LastEditTime: 2022-03-31 23:54:01 +0800
  * @LastEditors : JackChou
  */
 const express = require('express')
@@ -10,13 +10,17 @@ const nunjucks = require('nunjucks')
 const path = require('path')
 
 const app = express()
-app.set('view engine', 'njk')
+console.log(path.join(__dirname, 'static'))
+app.use(express.static(path.join(__dirname, 'static')))
+app.set('view engine', 'nunj')
 app.set('views', path.resolve(__dirname, './views'))
 
 nunjucks.configure('views', { autoescape: true, express: app })
 
 app.get('/', (req, res) => {
-  res.render('index', { name: 'postbird', title: 'nunjucks' })
+  res.render('index', { username: 'postbird', title: 'nunjucks' })
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('app listening on port 3000\n')
+})
